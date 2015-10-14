7c18cdc5b57a422e2869246508369a6d234d548c
668Jerry <668Jerry@gmail.com>
2015-10-14 09:49:06 +0800

    Initial Commit

---
+++ CPU Identifier/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CPU Identifier
+//
+//  Created by lololol on 14/Oct/15.
+//  Copyright © 2015 nouvel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CPU Identifier/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CPU Identifier
+//
+//  Created by lololol on 14/Oct/15.
+//  Copyright © 2015 nouvel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
