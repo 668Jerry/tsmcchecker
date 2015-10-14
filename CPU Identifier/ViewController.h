d5c6765f170c9116e005312601ed89b36aaec97f
668Jerry <668Jerry@gmail.com>
2015-10-14 11:14:38 +0800

    init

--- CPU Identifier/ViewController.h
+++ CPU Identifier/ViewController.h
@@ -8,7 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
+#include <sys/sysctl.h>
+#include <sys/types.h>
+#include <mach/mach.h>
+#include <mach/processor_info.h>
+#include <mach/mach_host.h>
+
 @interface ViewController : UIViewController
+{
+    processor_info_array_t cpuInfo, prevCpuInfo;
+    mach_msg_type_number_t numCpuInfo, numPrevCpuInfo;
+    unsigned numCPUs;
+    NSTimer *updateTimer;
+    NSLock *CPUUsageLock;
+}
 
 
 @end
--- CPU Identifier/ViewController.m
+++ CPU Identifier/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <sys/utsname.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    size_t size;
+    sysctlbyname("hw.model", NULL, &size, NULL, 0);
+    char *chModel = (char*)malloc(size);
+    sysctlbyname("hw.model", chModel, &size, NULL, 0);
+    NSString *modelName = [NSString stringWithCString:chModel encoding:NSUTF8StringEncoding];
+    free(chModel);
+    NSLog(@"modelName: %@", modelName);
+    UITextView *uitv = [[UITextView alloc]initWithFrame:CGRectMake(0, 100, 320, 200)];
+    [self.view addSubview:uitv];
+    ([modelName isEqualToString:@"N71AP"] || [modelName isEqualToString:@"N71mAP"])?
+    [uitv setText:[NSString stringWithFormat:@"你iphone的modelName是%@, 來自tsmc!!", modelName]]:
+    [uitv setText:[NSString stringWithFormat:@"你iphone的modelName是%@, 這不是tsmc!!", modelName]];
 }
 
 - (void)didReceiveMemoryWarning {
